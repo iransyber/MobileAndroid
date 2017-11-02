fedb682128fef5dd52f036131757d70501c0a1d5
Iran Junior <iranjunior@solucaosistemas.net>
2017-11-02 13:51:47 -0300

    Adicao de tabs

---
+++ app/src/main/java/com/example/iran/sysdvp/TabTodasMovimentacoes.java
@@ -0,0 +1,19 @@
+package com.example.iran.sysdvp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Iran on 02/11/2017.
+ */
+
+public class TabTodasMovimentacoes extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_1, container, false);
+    }
+}
